45bbcb81a13e4a2b70b836d85f31580b597ce16b
edwa1401 <edwa14011977@gmail.com>
2022-10-16 20:46:35 +0300

    add SQlite, save  picture title, path to local dir  and saving time to database

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,12 +1,17 @@
+from datetime import datetime
 from flask import Flask, render_template
+from flask_migrate import Migrate
 import os
 from werkzeug.utils import secure_filename
-from webapp.upload_form import Upload_picture_Form
 
+from webapp.upload_form import Upload_picture_Form
+from webapp.model import db, Photo
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
     @app.route('/uploader', methods=['POST'])
     def upload_pict():
@@ -16,9 +21,16 @@ def create_app():
             f = form.picture.data
             file_name = secure_filename(f.filename)
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-            return 'Файл загружен'
+            upload_time = datetime.now()
+            save_upload_info(file_name, app.config['UPLOAD_FOLDER'], upload_time)
+            return f'Файл {file_name} загружен'
         return 'Файл не загружен'
 
+    def save_upload_info(picture_title, path_to_dir, upload_time):
+        upload_info = Photo(picture_title=picture_title, path_to_dir=path_to_dir,
+                            upload_time=upload_time)
+        db.session.add(upload_info)
+        db.session.commit()
     @app.route('/', methods=['GET'])
     def index():
         form = Upload_picture_Form()
--- webapp/config.py
+++ webapp/config.py
@@ -1,5 +1,10 @@
 import os
 
-UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '../pictures')
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '../downloads')
 SECRET_KEY = "jwhdjwdhjwdkwhd7672629e292e928e928e"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
