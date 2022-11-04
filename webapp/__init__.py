248797bf8f07dbd48ec36edb377eb8456193b135
edwa1401 <edwa14011977@gmail.com>
2022-11-04 11:58:18 +0300

    add db migration

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 from webapp.db import db
 from webapp.feedback.views import blueprint as feedback_blueprint
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     app.register_blueprint(feedback_blueprint)
     app.register_blueprint(load_image_blueprint)
--- webapp/config.py
+++ webapp/config.py
@@ -5,3 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "../downloads")
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "..", "webapp.db")
 SECRET_KEY = "nruefur47h5nit58h4uu73h7^E%F^juehf"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
