912807b837cacb26687ae8ecfa44015035f45a27
Maria Romanchuk <mashulya_r_88@mail.ru>
2022-11-08 22:55:37 +0700

    Added footer

--- webapp/load_image/views.py
+++ webapp/load_image/views.py
@@ -4,6 +4,8 @@ import os
 
 from webapp.load_image.forms import UploadForm
 from webapp.load_image.utils import create_filename, save_upload_info
+# import predict_cat
+# import predict_braille
 
 blueprint = Blueprint("upload", __name__)
 
@@ -12,8 +14,9 @@ blueprint = Blueprint("upload", __name__)
 def index():
     title = "Braille Recognizer"
     upload_form = UploadForm()
-    url_photo = os.path.join(current_app.config["UPLOAD_FOLDER"], "photo.jpg")
+    url_photo = "splash_screen.jpg"
     translation_text = "Текст перевода"
+
     if upload_form.validate_on_submit():
         f = upload_form.image.data
         filename = create_filename()
@@ -21,6 +24,18 @@ def index():
         upload_time = datetime.now()
         save_upload_info(filename, current_app.config["UPLOAD_FOLDER"], upload_time)
         flash("Фотография успешно отправлена")
+        # its_cat = predict_cat.cat_predict(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
+        # its_braille = predict_braille.braille_predict(os.path.join(current_app.config["UPLOAD_FOLDER"],
+        #                                                            filename))
+
+        # if its_cat:
+        #     url_photo = os.path.join(current_app.config["UPLOAD_FOLDER"], filename)
+        #     translation_text = "Как мило, это же котик! Но пришлите, пожалуйста, фотогафию шрифта Брайля"
+        # elif its_braille:
+        #     url_photo = os.path.join(current_app.config["UPLOAD_FOLDER"], filename)
+        #     translation_text = "Что-нибудь"
+        # else:
+        #    translation_text = "К сожалению, мы не можем обнаружить на этой фотографии шрифт Брайля"
 
         return render_template("load_image/index.html", page_title=title, form=upload_form,
                                url_photo=url_photo, card_text=translation_text)
